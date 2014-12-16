2d59abe8c4da534bab2199439b5567e5fc50e9ff
gillbertS <ldes89150@gmail.com>
2014-12-16 19:21:38 +0800

    pack

---
+++ hw6/b01202065_hw6/compare
@@ -0,0 +1,3 @@
+time ./cirTest -f testall.do2 > usr.ref 2>&1 
+time ./ref/cirTest -f testall.do2 > ref.ref 2>&1
+vimdiff usr.ref ref.ref
---
+++ hw6/b01202065_hw6/pack.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+STUDENTID="b01202065";
+HOMEWORK_NUM="_hw6/";
+echo "Packing Homework "${STUDENTID}${HOMEWORK_NUM};
+if [ ! -d ${STUDENTID}${HOMEWORK_NUM} ]; then
+echo "Creating Directory "${STUDENTID}${HOMEWORK_NUM};
+mkdir ${STUDENTID}${HOMEWORK_NUM};
+else
+while true; do 
+read -p "Directory already exist! Continuing could distroy all previous
+files in derectory $STUDENTID${HOMEWORK_NUM}! Do you want to continue? (Y/n)" ans
+case $ans in
+[Yy]* ) break;;
+[Nn]* ) echo "Terminating packing program"; exit;;
+* ) echo "Please reply yes or no";;
+esac
+done
+fi
+echo -e "Copying files...";
+for line in $(cat ./MustExist.txt)
+do
+line=${line#$HOMEWORK_NUM};
+if [ -e ${STUDENTID}${HOMEWORK_NUM}${line} ] ; then
+echo -e "\nWarning! Overriding previous existence of
+"${STUDENTID}${HOMEWORK_NUM}$line;
+fi
+if [ ! -e ${line} ]; then
+echo -e "\nWarning! $line doesn't exist!";
+else 
+cp --parents $line ${STUDENTID}${HOMEWORK_NUM};
+fi
+done
+echo -e "\nStripping must remove files...";
+for line in $(cat ./MustRemove.txt)
+do
+if [ -e ${STUDENTID}${line} ] ; then
+echo "Removing redudent file $STUDENTID$line...";
+rm ${STUDENTID}${line};
+fi
+done
+echo "Compressing file";
+tar -zcf "${STUDENTID}${HOMEWORK_NUM%"/"}.tar.gz" "${STUDENTID}${HOMEWORK_NUM}";
+echo "Cleaning up";
+rm -r ${STUDENTID}${HOMEWORK_NUM}
+
+./SelfCheck "${STUDENTID}${HOMEWORK_NUM%"/"}.tar.gz"
+
