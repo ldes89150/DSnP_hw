5ccaac434e96e5e302c30f4d52a21d1d40587aba
gillbertS <ldes89150@gmail.com>
2014-12-27 22:19:47 +0800

    add hw7

---
+++ hw7/b01202065_hw7/src/util/myMinHeap.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+  FileName     [ myMinHeap.h ]
+  PackageName  [ util ]
+  Synopsis     [ Define MinHeap ADT ]
+  Author       [ Chung-Yang (Ric) Huang ]
+  Copyright    [ Copyleft(c) 2014 LaDs(III), GIEE, NTU, Taiwan ]
+****************************************************************************/
+
+#ifndef MY_MIN_HEAP_H
+#define MY_MIN_HEAP_H
+
+#include <algorithm>
+#include <vector>
+
+template <class Data>
+class MinHeap
+{
+public:
+   MinHeap() : _data(0) {}
+   MinHeap(size_t s) {
+      _data.reserve(s);
+      for (size_t i = 0; i < s; ++i) _data.push_back(Data());
+      sort(_data.begin(), _data.end());
+   }
+   ~MinHeap() {}
+
+   // For the following member functions,
+   // We don't respond for the case vector "_data" is empty!
+   const Data& operator [] (size_t i) const { return _data[i]; }   
+   Data& operator [] (size_t i) { return _data[i]; }
+
+   size_t size() const { return _data.size(); }
+
+   // TODO
+   const Data& min() const { return Data(); }
+   void insert(const Data& d) { }
+   void delMin() { }
+   void delData(size_t i) { } // remove _data[i]
+
+private:
+   // DO NOT add or change data members
+   vector<Data>   _data;
+};
+
+#endif // MY_MIN_HEAP_H
